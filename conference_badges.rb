7d087a08816f8daf507c8e563a24f5e3864a783f
oussama kahloul <oussama.kahloul.1996@gmail.com>
2017-03-12 20:05:46 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+# Write your code here. 
+
+def badge_maker(name)
+
+   "Hello, my name is #{name}."  
+end
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+ def batch_badge_creator(array)
+   
+   badge_messages = []
+   array.each do |name|
+     badge_messages <<    "Hello, my name is #{name}."  
+
+end
+badge_messages   
+ end
+
+ 
+ def assign_rooms(array)
+   
+   room_assignments =[]
+   array.each_with_index.each do |name, index|
+          room_assignments  <<     "Hello, #{name}! You'll be assigned to room #{index+1}!"
+   end
+   room_assignments
+ end
+
+
+ def printer(array)
+   batch_badge_creator(array).each do |badge|
+      puts badge
+   end
+   assign_rooms(array).each do |element|
+     puts element
+   end
+   
+ end
