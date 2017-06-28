8d89fb1119953a57f03d47c0e0ae10e69ef0a87c
Leslie Huang <lesliehuang@gmail.com>
2017-06-28 18:42:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  return names.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(names)
+  messages = Array.new
+
+  names.each_with_index do |name, index|
+    messages.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+
+  return messages
+
+end
+
+def printer(names)
+
+  batch_badge_creator(names).map{|msg| puts msg}
+  assign_rooms(names).map{|msg| puts msg}
+
+end
