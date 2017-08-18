02c829407800b42c6651fa4ed97891dc259bef0d
Jason Pham <jcpham1@uci.edu>
2017-08-18 00:16:32 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  index = 0
+  badges = []
+  names.each do |name|
+    badges.push("Hello, my name is #{name}.")
+    index += 1
+  end
+  return badges
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index do |speaker, index|
+    room_assignments[index] = "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  return room_assignments
+end
+
+def printer(names)
+  batch_badge_creator(names)
+  assign_rooms(names)
+  badges.each {|badge| puts "#{badge}"}
+  room_assignments.each {|room_assignment| puts "#{room_assignment}"}
+end
