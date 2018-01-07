719b8e17f392af731c50a9859f8df9107570590b
ivandelas <ivandelas@gmail.com>
2018-01-07 08:34:23 +0100

    done

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,13 +7,20 @@ end
 
 
 def batch_badge_creator(attendees)
-  badge = []
-  attendees.each { |name| badge << badge_maker(name) }
-  badge
+  badges = []
+  attendees.each { |name| badges << badge_maker(name) }
+  badges
 end
 
 def assign_rooms(attendees)
-  message = []
+  messages = []
   rooms = (1..7).to_a
-  attendees.each_with_index { |name, index| message << "Hello, #{name}! You'll be assigned to room #{index + 1}!" if attendees.size <= rooms.size }
+  attendees.each_with_index { |name, index| messages << "Hello, #{name}! You'll be assigned to room #{index + 1}!" if attendees.size <= rooms.size }
+  messages
+end
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |message| puts message }
 end
