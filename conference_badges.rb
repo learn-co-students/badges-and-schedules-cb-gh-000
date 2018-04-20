8e48ec9103a33f50cb266987847adccfacc3ae15
Caio Sanches <ccampato@gmail.com>
2018-04-20 05:08:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(array)
+  batch = Array.new
+  array.each do |speaker|
+    badge_message = badge_maker(speaker)
+    batch << badge_message
+  end
+  batch
+end
+
+def assign_rooms(array)
