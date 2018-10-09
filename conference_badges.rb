81f5bc9a37389abb365023d0310a5e38fdc35857
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-09 20:22:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,12 +20,10 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  badges = batch_badge_creator(array)
-  rooms = assign_rooms(array)
-  badges.each do |attendee|
+  batch_badge_creator(array).each do |attendee|
     puts attendee
   end
-  rooms.each do |room|
+  assign_rooms(array).each do |room|
     puts room
   end
 end
