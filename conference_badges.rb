be0ad75ebc70d65eb8e1bee96c19814988476e0a
Bryan Jimenez <bryan@roguedevs.io>
2019-04-08 08:30:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  badges = []
+  arr.each do |item|
+    badges << badge_maker(item)
+  end
+  badges
+end
+
+def assign_rooms(arr)
+  rooms = []
+  arr.each_with_index do |item, index|
+    rooms << "Hello, #{item}! You'll be assigned to room #{index+1}!"
+  end
+  rooms
+end
+
+def printer(arr)
+  batch_badge_creator(arr).each do |id|
+    puts id
+  end
+
+  assign_rooms(arr).each do |id|
+    puts id
+  end
+end
